6ff6b6c68f44b0bace2ac18f5dddce13e99fa00d
apellidostack <apellidojoan4@gmail.com>
2025-05-08 12:18:58 -0400

    commit

---
+++ src/main/java/com/prueba/tecnica/prueba_tecnica/repositories/FoodRepository.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.prueba_tecnica.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prueba.tecnica.prueba_tecnica.entities.Food;
+
+@Repository
+public interface FoodRepository extends CrudRepository<Food, Long>{
+    List<Food> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String title, String description);
+}
